6d257241afbef0264f9fc5dd6d72a0b5e9bd6a88
Rahul Bansiwal <rahulbnswl7@gmail.com>
2023-04-19 15:23:26 +0400

    addded project folder structure MVC based design pattern

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const authRoutes =  require('../routes/auth');
+
 const app = express();
 require("dotenv").config();
 
 app.use(bodyParser.json());
 
-
+app.use('/auth',authRoutes);
 app.listen(process.env.PORT_NUMBER,(err:Error)=>{
     if(err)
     console.log(err);
---
+++ src/routes/auth.js
@@ -0,0 +1,4 @@
+"use strict";
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const router = require('express').router();
