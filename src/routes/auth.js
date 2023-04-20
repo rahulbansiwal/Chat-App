d63931ba34654b03cae5200995fe8a0f6d7c45f0
rahulbansiwal <rahulbnswl7@gmail.com>
2023-04-21 00:53:37 +0400

    removed typescript

--- src/routes/auth.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const router = require('express').router();
--- src/routes/auth.ts
+++
@@ -1,6 +0,0 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const router = require('express').Router();
-const authControllers = require('../controllers/authControllers');
-
-router.route('/signup').post(authControllers.signup);
