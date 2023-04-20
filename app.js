6f32588e8cb2c188ed0c51b8f4d0eb469d31ae55
rahulbansiwal <rahulbnswl7@gmail.com>
2023-04-21 00:59:18 +0400

    added MVC folder structure

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
+const app = express();
+require("dotenv").config();
+app.use(bodyParser.json());
+app.use('/auth', authRoutes);
+app.listen(process.env.PORT_NUMBER, (err) => {
+    if (err)
+        console.log(err);
+    else
+        console.log(`Server listening on port ${process.env.PORT_NUMBER}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,6 @@
+"use strict";
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const router = require('express').Router();
+const authControllers = require('../controllers/authControllers');
+router.route('/signup').post(authControllers.signup);
